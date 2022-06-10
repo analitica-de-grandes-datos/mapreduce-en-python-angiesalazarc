01929ce9e7a9470e46ccf53fd80be48d93467bec
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-09 21:54:50 -0500

    Punto 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -5,5 +5,5 @@ import sys
 
 if __name__ == "__main__":
     for line in sys.stdin:
-      esp = line.strip().split(",")
+      esp = line.strip().split(",",3)
       sys.stdout.write("{}\t1\n".format(esp[2]))
