76ae77b4409867de2d5b8c243bc813d0eba0d09d
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-10 22:03:41 -0500

    Punto 2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+     purpose = line.split(",")[3]
+     amount = line.split(",")[4]
+     
+     sys.stdout.write("{}\t1\n".format(purpose, amount))
