2bd9aed72c90e863d339ca93ec388aeb9452da2c
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-11 21:42:16 -0500

    Punto 4

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        column = line.split(" ")[0]
+        sys.stdout.write("{}\t1\n".format(column))
+
+        
