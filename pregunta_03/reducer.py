d44f797911e5e42c3f77382377db27e3f22685ea
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-11 20:42:02 -0500

    Punto 3

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -4,17 +4,10 @@
 import sys
 
 if __name__ == "__main__":
-    curkey = None
-    value = 0
 
     for line in sys.stdin:
-        key, val = line.split("\t")
-
-        if curkey is not None:
-            sys.stdout.write("{},{}\n".format(value.replace("\n",""),curkey))
-        curkey = key
-        value = val
-    
-    sys.stdout.write("{},{}".format(value.replace("\n",""),curkey))
+        lista = line.split(",")
+        
+        sys.stdout.write("{},{}\n".format(lista[1].strip(),lista[0]))
 
         
