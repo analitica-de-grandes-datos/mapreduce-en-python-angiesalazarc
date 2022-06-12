b867773837884ec3eb948fbcae4a26ad76513e80
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-11 20:19:28 -0500

    Punto 3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letter = line.split(",")[0]
+        value = line.split(",")[1]
+        value = int(value)
+
+        sys.stdout.write("{}\t{}\n".formmat(letter,value))
