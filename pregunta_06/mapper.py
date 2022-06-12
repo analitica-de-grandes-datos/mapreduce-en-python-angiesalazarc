bf7f57ecb9c8b6be542fb2f90f29e1a9856b5cc7
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 18:25:55 -0500

    Punto 6

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        column1 = line.split(" ")[0]
+        column2 = line.split(" ")[2]
+
+        sys.stdout.write("{}\t{}\n".format(column1, float(column2)))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    curkey = None
+    max = 0
+
+    for line in sys.stdin:
+        key, value = line.split("\t")
+        value = float(value)
+
+        if key == curkey:
+            if value > max:
+                max = value
+            if value < min:
+                min = value
+
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
+            curkey = key
+            max = value
+            min = value
+
+        sys.stdout.write("{}\t{}\t{}\n".format(curkey, max, min))
