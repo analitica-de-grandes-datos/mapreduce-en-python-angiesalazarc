c32aca12fac813ecccca8203edcb611b18d1a432
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 20:31:33 -0500

    Punto 8

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letter = line.split("  ")[0]
+        value = line.split("  ")[2]
+        value = int(value)
+        
+        sys.stdout.write("{}\t{}\n".format(letter, value))
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    curkey = None
+    count = 0
+
+    for line in sys.stdin:
+
+        key, value = line.split("\t")
+        value = int(value)
+
+        if key == curkey:
+            t = t + value
+            count = count + 1
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, t, t/count))
+            curkey = key
+            t = value
+
+        sys.stdout.write("{}\t{}\t{}\n".format(curkey, t, t/count))  
