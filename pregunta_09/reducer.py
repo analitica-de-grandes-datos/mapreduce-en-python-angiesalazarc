e4909f18346bdb342119249856148fb059b5384c
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 21:10:01 -0500

    Punto 9

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    contador = 0
+
+    for line in sys.stdin:
+        lista = line.split("  ")
+
+        sys.stdout.write("{}   {}   {}\n".format(lista[0], lista[1].strip(), int(lista[2])))
+
+        if contador >= 5:
+            break
+
+        contador = contador + 1
