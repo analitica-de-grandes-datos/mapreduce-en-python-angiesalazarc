b94066e246a611179a6276340e87a7a14e8f918f
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 20:06:52 -0500

    Punto 7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letter = line.split("  ")[0]
+        date = line.split("  ")[1]
+        value = line.split("  ")[2]
+        value = int(value)
+        
+        sys.stdout.write("{}\t{}\t{}\n".format(letter, date, value))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    vector = []
+
+    for line in sys.stdin:
+        letter, date, value = line.split("\t")
+        value = int(value)
+        vector.append((letter, date, value))
+    vector.sort(key=lambda orden: (orden[0], orden[2]))
+
+    for line in vector:
+        sys.stdout.write("{}   {}   {}\n".format(line[0], line[1].strip(), line[2]))
