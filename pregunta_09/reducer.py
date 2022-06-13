8a6e962f46ba5df1e62e4460fae7239bc7ac9dfd
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 21:11:47 -0500

    Punto 9

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -7,11 +7,10 @@ if __name__ == "__main__":
     contador = 0
 
     for line in sys.stdin:
-        lista = line.split("  ")
+        lista = line.split('   ')
 
-        sys.stdout.write("{}   {}   {}\n".format(lista[0], lista[1].strip(), int(lista[2])))
-
+        sys.stdout.write("{}   {}   {}\n".format(lista[1], lista[2].strip(), int(lista[0])))
+        
         if contador >= 5:
             break
-
-        contador = contador + 1
+        contador += 1
