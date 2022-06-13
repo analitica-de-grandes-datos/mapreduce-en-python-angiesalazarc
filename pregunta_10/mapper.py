c6ba29c483ef64593772fd291e1b98bcf5b4645c
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-06-12 22:05:17 -0500

    Punto 10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    vec = []
+
+    for line in sys.stdin:
+        value = line.split("\t")[0]
+        letter = line.split("\t")[1]
+
+        for i in letter.split(","):
+            vec.append((value, i))
+    
+    for line in vec:
+        sys.stdout.write("{}\t{}\n".format(line[1].strip(), line[0].strip()))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    vector = []
+
+    for line in sys.stdin:
+        letter, value = line.split("\t")
+        value = int(value)
+        vector.append((letter, value))
+
+    vector.sort(key=lambda orden: (orden[0], orden[1]))
+
+    curkey = None
+
+    for line in vector:
+        if curkey == line[0]:
+            sys.stdout.write(",{}".format(line[1]))
+        else:
+            if curkey is not None:
+                sys.stdout.write("\n{}\t{}".format(line[0], line[1]))
+                curkey = line[0]
+            else:
+                sys.stdout.write("{}\t{}".format(line[0], line[1]))
+                curkey = line[0]
